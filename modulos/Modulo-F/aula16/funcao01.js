862a7be050537dc741ca74642c4b7f6150ab9d0c
caioparrillo <caioluca944@gmail.com>
2026-09-04 20:35:23 -0300

    Funções utilizando parâmetros

---
+++ modulos/Modulo-F/aula16/funcao01.js
@@ -0,0 +1,15 @@
+function parimpar(n){
+    if (n%2==0){
+        return 'Par!'
+    }else{
+        return 'Ímpar!'
+    }
+}
+
+/*  1º forma de mostrar o resultado
+let res = parimpar(4)
+console.log(res)
+*/
+
+//2º forma de mostrar o resultado
+console.log(parimpar(15))
---
+++ modulos/Modulo-F/aula16/funcao05.js
@@ -0,0 +1,19 @@
+//RECURSIVIDADE
+// fatorial recursiva, uma recursao acontece quando a função chama ela mesma
+
+function fatorial(n){
+    if(n==1){
+        return 1
+    }else{
+        return n * fatorial(n-1)
+    }
+} 
+console.log(fatorial(5))
+
+/* 
+5! = 5x4x3x2x1
+5! = 5x4!
+
+n! = n x (n-1)!
+1! = 1
+*/
